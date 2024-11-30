c946931e7968f599597c6fefe4b17a3ab7513aff
Moataz Mohamed <157921519+moatazmohaamed@users.noreply.github.com>
2024-11-30 23:35:32 +0200

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,15 @@
+const username = document.getElementById(`name`)
+const logOutBtn = document.getElementById("logOutBtn");
+
+const user = localStorage.getItem("userName");
+username.innerHTML += user;
+
+logOutBtn.addEventListener("click", function () {
+    localStorage.removeItem(`userName`)
+    location.href = './index.html';
+})
+
+const href = location.href;
+if (user === null && href.includes('/home')) {
+    location.href = '/index.html';
+}
